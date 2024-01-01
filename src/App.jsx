fb1d7dcc2c98410161c24bf8cd614e99511577c2
tomiwadotJS <victorgbayedev@gmail.com>
2024-01-01 21:13:24 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Auth/Login/Login';
 import Discover from './pages/Discover/Discover'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Playlist from './pages/Playlist/Playlist'
+import Signup from './pages/Auth/Signup/Signup';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Discover/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
           <Route path="/playlist" element={ <Playlist/>}/>
         </Routes>
       </Router>
