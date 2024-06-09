ac8f3d20896b5ba4b75e90e72ddea727374a1507
tomiwadotJS <victorgbayedev@gmail.com>
2024-06-09 12:20:48 +0100

    changes to sidebar logic

--- src/component/compound/Sidebar/Sidebar.jsx
+++ src/component/compound/Sidebar/Sidebar.jsx
@@ -1,17 +1,19 @@
 import { SidsebarData } from "./SidebarData"
 import styles from './Sidebar.module.scss'
 import logo from '../../../assets/agbalumotunesLogo.svg'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <div className={styles.SidebarContainer}>
         <div className={styles.SidebarLogo}>
-            <img src={logo}/>
+            <img src={logo} alt="Agbalumo Tunes Logo"/>
         </div>
         <section  className={styles.SidebarListItemContainer}>
             {SidsebarData.map((data)=>{
-                const isActive = location.pathname === `/${data.url}`;
+                const isActive = location.pathname === `/${data.url}` || (location.pathname === '/' && data.url === 'discover');
                 return(
                     <main key={data.id}>
                         <Link to={`/${data.url}`}
@@ -19,10 +21,10 @@ const Sidebar = () => {
                         >
                             <div className={`${styles.SidebarListItemGroup} ${isActive ? styles.active : ''}`}>
                                 <div className={styles.SidebarListItem}>
-                                    <img src={data.icon}/>
+                                    <img src={data.icon} alt={`${data.text} icon`}/>
                                     <p>{data.text}</p>
                                 </div>
-                                <div className={isActive ? styles.verticalLine: ''}></div>
+                                <div className={isActive ? styles.verticalLine : ''}></div>
                             </div>
                         </Link>
                     </main>
